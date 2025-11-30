487788627b4d16a09941852880f20cf1c8b80d35
SoumyaBaranwal24 <soumyabaranwal62@gmail.com>
2025-11-30 13:04:32 +0000

    conversion of data

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,31 @@
+let score = "33billionaire"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+
+// "33" => 33 // NaN means not a no.
+// "33billionaire" =>NaN
+// true => 1, false => 0
+
+let isloggedin = "soumya"
+console.log(typeof isloggedin);
+console.log(isloggedin);
+
+let booleanOfIsloggedIn = Boolean(isloggedin)
+console.log(typeof booleanOfIsloggedIn);
+console.log(booleanOfIsloggedIn)
+
+// 1=>true, 0 =>false
+// "" => false
+// "soumya" => true
+  
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
